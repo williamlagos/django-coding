053a4fc03d80a3311826d22c27755f4ddc3374a2
William Oliveira de Lagos <william.lagos@live.com>
2014-02-28 19:08:09 -0300

    Added first migrated code

---
+++ hub/models.py
@@ -0,0 +1,36 @@
+#
+# This file is part of Efforia project.
+#
+# Copyright (C) 2011-2013 William Oliveira de Lagos <william@efforia.com.br>
+#
+# Efforia is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Efforia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Efforia. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from django.db.models import *
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.template import Context,Template
+from datetime import date
+
+locale = settings.LOCALE_DATE
+
+class Place(Model):
+    name = CharField(default="",max_length=50)
+    user = ForeignKey(User,unique=True)
+    code = CharField(default="",max_length=100)
+    city = CharField(default="",max_length=100)
+    country = CharField(default="",max_length=50)
+    date = DateTimeField(default=date.today(),auto_now_add=True)
+
+User.place = property(lambda p: Place.objects.get_or_create(user=p)[0])

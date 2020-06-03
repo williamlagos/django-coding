e1f158c9a1e8fecedc86f2b2637000857bdb94ea
William Oliveira de Lagos <william.lagos@icloud.com>
2020-06-02 21:10:57 -0300

    Updated manage script on main project for pandora hub

--- pandora-hub/web/manage.py
+++ pandora-hub/web/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 #
 # This file is part of Efforia project.
 #
@@ -18,9 +18,25 @@
 # along with Efforia. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import os,sys,subprocess
+#!/usr/bin/env python
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "efforia.settings")
-    from django.core.management import execute_from_command_line
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandora.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
